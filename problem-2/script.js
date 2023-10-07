956b1b5f17c52dbccfff142e541482067b82e5fc
GonzoTheDev <c20703429@mytudublin.ie>
2023-10-07 20:09:05 +0100

    Adding comments

--- problem-2/script.js
+++ problem-2/script.js
@@ -1,5 +1,7 @@
+// Set the default colour to be pastel red
 let selectedColor = '#ffcccb';
 
+// Function to detect and handle requests to change the colour of notes
 function changeColor(color, elementId) {
     selectedColor = color;
     const prevColorOption = document.getElementsByClassName('selected-color')[0];
@@ -11,20 +13,25 @@ function changeColor(color, elementId) {
     colorOption.className = 'selected-color'
 }
 
+// Function to add a new note to the page
 function addNote() {
+
+    // Get the notes container
     const notesContainer = document.getElementById('notes-container');
 
+    // Create the note and set the background to selected colour
     const note = document.createElement('div');
     note.className = 'note';
     note.style.backgroundColor = selectedColor;
 
+    // Create the text area, set the placeholder and make it editable
     const textAreaContainer = document.createElement('div');
     textAreaContainer.className = 'editable';
-
     const textArea = document.createElement('textarea');
     textArea.placeholder = 'Enter your note...';
     textAreaContainer.appendChild(textArea);
 
+    // Create the content div and set the text to the text area value
     const contentDiv = document.createElement('div');
     contentDiv.className = 'note-content';
     contentDiv.innerText = textArea.value;
@@ -32,12 +39,14 @@ function addNote() {
     const buttonsContainer = document.createElement('div');
     buttonsContainer.className = 'note-buttons';
 
+    // Functionality to delete the note
     const deleteBtn = document.createElement('button');
     deleteBtn.innerText = 'Delete';
     deleteBtn.onclick = function () {
         notesContainer.removeChild(note);
     };
 
+    // Functionality to save and edit the note
     const editBtn = document.createElement('button');
     editBtn.innerText = 'Save';
     editBtn.onclick = function () {
@@ -55,12 +64,16 @@ function addNote() {
         }
     };
 
+    // Append the buttons to the buttons container
     buttonsContainer.appendChild(deleteBtn);
     buttonsContainer.appendChild(editBtn);
 
+    // Append the text area, content div and buttons container to the note
     note.appendChild(textAreaContainer);
     note.appendChild(contentDiv);
     note.appendChild(buttonsContainer);
 
+    // Append the note to the notes container
     notesContainer.appendChild(note);
+    
 }
