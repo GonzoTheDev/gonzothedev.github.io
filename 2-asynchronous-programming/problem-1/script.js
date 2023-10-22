cb74aed230a7f2a12e9e841d7135a201b59b263f
GonzoTheDev <c20703429@mytudublin.ie>
2023-10-22 15:46:26 +0100

    undo mistakenly removed feature

--- 2-asynchronous-programming/problem-1/script.js
+++ 2-asynchronous-programming/problem-1/script.js
@@ -75,9 +75,9 @@ var contacts = [];
       var searchValue = this.value.trim().toLowerCase();
       var table = document.getElementById('contactsTable');
       var rows = table.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
-      var errorDiv = document.getElementById('error');
+      var noResultDiv = document.getElementById('noResult');
 
-      errorDiv.style.display = 'none';
+      noResultDiv.style.display = 'none';
 
       for (var i = 0; i < rows.length; i++) {
         var mobileValue = rows[i].getElementsByTagName('td')[1].innerText.toLowerCase();
